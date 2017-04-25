ae8637e45f1e6363a29192a8c7b7720fc15b2188
hyun-song <99hyunsong@gmail.com>
2017-04-25 13:47:03 -0700

    Rename APIntList.java to APINtList.java

---
+++ APINtList.java
@@ -0,0 +1,21 @@
+public interface APIntList {
+	int size();
+	void add(int item);
+	void add(int index, int item);
+	void set(int index, int item);
+	int get(int index);
+	
+	/**
+	 * Uncomment me if you're doing extra credit
+	 * and replace with appropriate documentation
+	 */
+	// void clear();
+	
+	/**
+	 * Uncomment me if you're doing extra credit
+	 * and replace with appropriate documentation
+	 * @param index
+	 * @return
+	 */
+	// int remove(int index);
+}
